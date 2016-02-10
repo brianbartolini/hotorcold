8e6502f921af4c736c279d07d9da39204767c77f
brianbartolini <bmbartolini@gmail.com>
2016-02-09 22:19:07 -0800

    updated

--- js/app.js
+++ js/app.js
@@ -40,50 +40,66 @@ window.onload = function newGame(){
   		
   });
 
-  // User inputs guess and difference between random number and difference is calculated.
+  // User inputs guess and difference between random number and guess is calculated.
 
   $(document).on('click', "#guessButton", function(){		
   		
+
   		input = document.getElementById("userGuess").value;
-  		$("#guessList").append("<li>" + input + "</li>");
   		inputNum = parseInt(input, 10);
-  		difference = Math.abs(randomNumber-inputNum);
-		clickCount += 1;
+  		
+  		if (inputNum > 100 || input == "") {
+  			alert("please type a number between 0 and 100")
+  		
+  		}
+
+  		else{
+
+		difference = Math.abs(randomNumber-inputNum);
+
+
+		// Add guess to list and click count;
+
+  		$("#guessList").append("<li>" + input + "</li>");
+
+    	clickCount += 1;
 		$("#count").replaceWith("<span id='count'>" + clickCount + "</span>");
 
  // If guess is greater than 50 from random number then alert ice cold.
 	
 
-	if (difference > 50) {
-		$("#feedback").replaceWith("<h2 id='feedback'>Ice-Cold</h2>");
-	}
+			if (difference > 50) {
+				$("#feedback").replaceWith("<h2 id='feedback'>Ice-Cold</h2>");
+			}
 
  // If between 30-50 alert cold
 	
-	else if (difference > 30) {
-		$("#feedback").replaceWith("<h2 id='feedback'>Cold</h2>");
-	}
+			else if (difference > 30) {
+				$("#feedback").replaceWith("<h2 id='feedback'>Cold</h2>");
+			}
 
 // If between 20 and 30, warm
 
-	else if (difference > 20) {
-		$("#feedback").replaceWith("<h2 id='feedback'>Warm</h2>");
-	}
+			else if (difference > 20) {
+				$("#feedback").replaceWith("<h2 id='feedback'>Warm</h2>");
+			}
 
 // if between 10 and 20, hot
 
-	else if (difference > 10) {
-		$("#feedback").replaceWith("<h2 id='feedback'>Hot</h2>");
-	}
+			else if (difference > 10) {
+				$("#feedback").replaceWith("<h2 id='feedback'>Hot</h2>");
+			}
 
-	else if (difference > 0) {
-		$("#feedback").replaceWith("<h2 id='feedback'>Very Hot</h2>");
-	}
+			else if (difference > 0) {
+				$("#feedback").replaceWith("<h2 id='feedback'>Very Hot</h2>");
+			}
 // If between 1 and 10 very hot
 	
-	else {
-		$("#feedback").replaceWith("<h2 id='feedback'>Correct</h2>");
-	}
+			else {
+				$("#feedback").replaceWith("<h2 id='feedback'>Correct</h2>");
+			}
+}
+
 }); 
 
 // Reset the screen when new game is clicked.
